d4d27029ee96efbaa57e9cb5d569adc63f84e119
Jesse Waddell <waddelljesse1@gmail.com>
2018-09-12 10:48:42 -0500

    returned null but no errs

--- sendinfo.js
+++ sendinfo.js
@@ -3,5 +3,6 @@ console.log('hello');
 //retrieveing document
 function getdocument() {
 var docc = document.getElementById('getit');
+return docc;
 }
-console.log(getdocument)
+console.log(getdocument())
