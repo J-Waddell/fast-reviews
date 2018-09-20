4825649669754e35b07f8a0c1d99a5a74b0732a6
Jesse Waddell <waddelljesse1@gmail.com>
2018-09-20 11:45:07 -0500

    js issues

--- sendinfo.js
+++ sendinfo.js
@@ -1,8 +1,9 @@
 console.log('hello');
 
-var doclink = document.getElementById('getit').value;
+var doclink = document.getElementById('GetIt');
 //retrieveing document
 function getdocument() {
+alert(doclink)
 return doclink;
 }
-console.log(getdocument(doclink), "here")
+console.log(getdocument(), "here")
